eeb918d4a41ac0e86fed24d0b01e861853b987e7
akhlil03uoh <akhleshrajpoot4@gmail.com>
2025-06-24 12:31:42 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,28 @@
+"""
+URL configuration for Website project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from webApp import views
-from django.urls import path
-from . import views
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('Base/', views.Base, name='Base'),  # ✅ 'base' name defined here
-    # The 'home' name is defined in the Home view below
-    # path('Home/', views.Home, name='Home'),  # This line is redundant if the Home view is already defined with the same name
-    # If you want to keep it, you can uncomment it, but it's not necessary since 'Home' is already defined below
-    path('', views.Home, name='Home'),  # ✅ 'home' name defined here
-    path('About/', views.About, name='About'),
-    path('Projects/', views.Projects, name='Projects'),
-    path('Resume/', views.Resume, name='Resume'),
-    path('Contact/', views.Contact, name='Contact'),
+    path('admin/', admin.site.urls),
+    path('', include('webApp.urls')), 
+    path('webApp/', include('webApp.urls')),
+    # path('webApp/', include('webApp.urls')),  # This line is redundant if the first line already includes webApp.urls
 ]
